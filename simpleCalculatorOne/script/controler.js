81b59ab2912fa2fe92f3836c93e79f42be1100ec
Dr Dmitriy Mishchuk <Mishchukltd@gmail.com>
2020-07-21 02:43:16 +0300

    Финальній комит простого калькулятора One (базовый вариант)

--- simpleCalculatorOne/script/controler.js
+++ simpleCalculatorOne/script/controler.js
@@ -6,5 +6,11 @@ const equal = () => {
   if(exp){
     document.form.textview.value = eval(document.form.textview.value);
   }
-
+}
+const clean = () => {
+  document.form.textview.value = "";
+}
+const back = () => {
+  let exp = document.form.textview.value;
+  document.form.textview.value = exp.substring(0, exp.length-1);
 }
