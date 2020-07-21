f7fecaaae55db746a1f28fbae9c1a8f98140ba94
Dr Dmitriy Mishchuk <Mishchukltd@gmail.com>
2020-07-21 15:03:54 +0300

    Завершил базовый калкулятор Two

--- simpleCalculatorTwo/script/controler.js
+++ simpleCalculatorTwo/script/controler.js
@@ -6,10 +6,28 @@ let input2 = document.getElementById('numberTwo');
 function getNumber(props){
   return Number(props.value);
 }
+function makeOperation(operation){
+  if (operation === '+') {
+    return getNumber(input1) + getNumber(input2);
+  }
+  else if (operation === '-') {
+    return getNumber(input1) - getNumber(input2);
+  }
+  else if (operation === '/') {
+    return getNumber(input1) / getNumber(input2);
+  }
+  else if (operation === '*') {
+    return getNumber(input1) * getNumber(input2);
+  }
+}
 
-const onButtonClick = () => {
 
-  alert(getNumber(input1) + getNumber(input2));
+const onButtonClick = (event) => {
+  let buttonText = event.currentTarget.innerHTML
+  alert(makeOperation(buttonText));
 }
 
 buttonPlus.addEventListener('click', onButtonClick);
+buttonMinus.addEventListener('click', onButtonClick);
+buttomDevide.addEventListener('click', onButtonClick);
+buttonMultiply.addEventListener('click', onButtonClick);
